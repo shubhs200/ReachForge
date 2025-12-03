fc4cc78f906c953ea3eb1c3054cf3bb53a00ee5b
Shubham Tiwari <tiwari2shubham@gmail.com>
2025-12-03 02:17:49 +0000

    updating compile command

--- config/compile_command.py
+++ config/compile_command.py
@@ -52,7 +52,7 @@ APP_RULES = {
     ),
     "challenge": (
         'bash -lc "AFL_USE_ASAN=1 '
-        'afl-clang-fast -g -O0 -fno-omit-frame-pointer -fsanitize=address,undefined '
+        'afl-clang -g -O0 -fno-omit-frame-pointer -fsanitize=address,undefined '
         '-I. -Iapp/src -Ibuild/vcpkg_installed/x64-linux-ellf/include '
         '{src} '
         '-o {binary} '
