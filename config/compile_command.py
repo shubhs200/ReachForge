a7de37d2b2330701edfb7581a5742e6075d2195a
Shubham Tiwari <tiwari2shubham@gmail.com>
2025-12-15 18:32:38 +0000

    updating the compile command so it finds the inlcudes and libs correctly

--- config/compile_command.py
+++ config/compile_command.py
@@ -118,12 +118,12 @@ APP_RULES = {
     '-Ideps/fast_float '
     '-Ideps/fpconv '
     '-Ideps/linenoise '
-    '-Ibuild-artifacts/vcpkg_installed/x64-linux-cromulence/include '
-    '-Ibuild-artifacts/vcpkg_installed/x64-linux-cromulence/include/hdr '
-    '-Ibuild-artifacts/vcpkg_installed/x64-linux-cromulence/include/hiredis '
+    '-Ibuild-artifacts/vcpkg_installed/x64-linux-ellf/include '
+    '-Ibuild-artifacts/vcpkg_installed/x64-linux-ellf/include/hdr '
+    '-Ibuild-artifacts/vcpkg_installed/x64-linux-ellf/include/hiredis '
     '{src} '
     '-o {binary} '
-    '-Lbuild-artifacts/vcpkg_installed/x64-linux-cromulence/lib '
+    '-Lbuild-artifacts/vcpkg_installed/x64-linux-ellf/lib '
     '-llua -lm -ldl -lpthread"'
     )
 }
