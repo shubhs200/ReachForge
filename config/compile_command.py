f035cd1d356b37c7cbbdf81437e8b6d57a3ea88d
Shubham Tiwari <tiwari2shubham@gmail.com>
2025-12-17 20:26:47 +0000

    updating compile command for redis

--- config/compile_command.py
+++ config/compile_command.py
@@ -120,12 +120,12 @@ APP_RULES = {
     '-Ideps/fast_float '
     '-Ideps/fpconv '
     '-Ideps/linenoise '
-    '-Ibuild/vcpkg_installed/x64-linux-cromulence/include '
-    '-Ibuild/vcpkg_installed/x64-linux-cromulence/include/hdr '
-    '-Ibuild/vcpkg_installed/x64-linux-cromulence/include/hiredis '
+    '-Ibuild-artifacts/vcpkg_installed/x64-linux-cromulence/include '
+    '-Ibuild-artifacts/vcpkg_installed/x64-linux-cromulence/include/hdr '
+    '-Ibuild-artifacts/vcpkg_installed/x64-linux-cromulence/include/hiredis '
     '{src} '
     '-o {binary} '
-    '-Lbuild/vcpkg_installed/x64-linux-cromulence/lib '
+    '-Lbuild-artifacts/vcpkg_installed/x64-linux-cromulence/lib '
     '-llua -lm -ldl -lpthread"'
     ),
 
