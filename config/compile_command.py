b1b7a50bf8cb64ef9021fa0f4f02828ff807ad07
Shubham Tiwari <tiwari2shubham@gmail.com>
2025-12-17 01:41:47 +0000

    fuxubg typo

--- config/compile_command.py
+++ config/compile_command.py
@@ -100,7 +100,7 @@ APP_RULES = {
     '../../build-artifacts/challenge/build/vcpkg_installed/x64-linux-ellf/lib/libbz2.a '
     '../../build-artifacts/challenge/build/vcpkg_installed/x64-linux-ellf/lib/libcrypto.a '
     '../../build-artifacts/challenge/build/vcpkg_installed/x64-linux-ellf/lib/libssl.a '
-    '../../build-artifacts/challenge/build/vcpkg_installed/x64-linux-ellf/lib/libzma.a '
+    '../../build-artifacts/challenge/build/vcpkg_installed/x64-linux-ellf/lib/liblzma.a '
     '-ldl -lm"'
     ),
 
